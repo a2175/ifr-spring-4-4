4932a6b97a5581375b6f07a2e265c2dd6d64ac7c
a2175 <as217512@gmail.com>
2023-12-10 21:07:07 +0900

    V2 - Validator 분리1
    ItemValidator 직접 호출하기

---
+++ src/main/java/hello/itemservice/web/validation/ItemValidator.java
@@ -0,0 +1,32 @@
+package hello.itemservice.web.validation;
+
+import hello.itemservice.domain.item.Item;
+
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.validation.BindingResult;
+
+@Component
+public class ItemValidator {
+
+    public void validate(Item item, BindingResult bindingResult) {
+
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() > 9999) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
+            }
+        }
+    }
+}
